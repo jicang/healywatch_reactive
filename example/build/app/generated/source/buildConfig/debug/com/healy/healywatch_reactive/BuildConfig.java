9fb07b60ad4b2f899972933e13c935e2daa2af82
Administrator <380677714@qq.com>
2022-02-25 15:51:05 +0800

    更改结构

---
+++ example/build/app/generated/source/buildConfig/debug/com/healy/healywatch_reactive/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.healy.healywatch_reactive;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.healy.healywatch_reactive";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
